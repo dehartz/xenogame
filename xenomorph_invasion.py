a43e029867ecee7ade45c7052db759021f72035c
dehartz <dehartz123@gmail.com>
2016-09-23 14:44:41 -0400

    Fleshed out journey_2()
    Added another morale decision to journey_2() added placeholder for
    door() which will contain puzzle of some sort.

--- xenomorph_invasion.py
+++ xenomorph_invasion.py
@@ -6,7 +6,6 @@ from sys import exit
 print "You are about to initialize into XENOMORPH INVASION."
 
 name = raw_input("What is your name? \n > ")
-morale = 0
 def start():
     print " You are Major %s" % name
     print """
@@ -58,7 +57,7 @@ def start():
 
 
 def journey_1():
-    prompt
+    morale = 0
     print """
     It worked! You hear the cries of pain and the gernade shrapnel rips through
     their chitinous exoskelton."""
@@ -73,17 +72,82 @@ def journey_1():
         if "1" in answer:
             morale += 1
             print "Sansa smiles at you, she looks reassured.\n"
-            journey_2()
+
+            continue_game()
         elif "2" in answer:
             morale -= 1
-            print "Sansa looks at you for a half second longer, then looks down at her,"
+
+            print "Sansa looks at you for a half second longer, then looks down at her"
             print "feet and walks ahead."
-            journey_2()
+            continue_game()
         else:
             print "Answer the question, stupid."
 
+
 def journey_2():
-    print "This is where journey 2 happens"
+    morale_2 = 0
+    text = "wigio"
+    for i in range (0,25):
+        print "\n"
+    print """
+    At the end of the hallway you come to a door.  On it is a jumble of 5 letters
+    the text reads %s.  Above the text is an engraving of a muscular man with
+    eyes like dark pools.  In his hand he has something round in shape, an apple?
+    \"Looks like the door needs a password\" Pvt.Williams says in his nearly
+    unintelligible southern accent.  \"Indeed, you driveling galooot.\" Xanarra
+    fires back hotly.  She was clearly nearing her wits' end.  You turn and look at Xanarra.
+    """ % text
+    print "\n\t You don't appreciate Xanarra speaking so harshly to your crew..."
+    print "But how do you want to handle this?\n"
+    print """
+    1.Rebuke Xanarra and remind her who is in charge.
+    2.Try to calm everyone down, we need to be working as a team.
+    """
+    answer2 = raw_input("> ")
+    while True:
+        if "1" in answer2:
+            morale_2 -= 1
+            print """
+            \"Xanarra! I don't give a damn who you are back at Federation One
+            Out here you answer to me, and I don't EVER want to hear you talk
+            to a member of my team like that again. Is that clear?\"
+
+            Shanarra holds your gaze, her eyes hard as flint.  After a moment
+            she looks away, saying nothing.
+            """
+            continue_game2()
+
+        elif "2" in answer2:
+            morale_2 += 1
+            print """
+            \"Guys, I know everyone is tired.  I am too.  Let's try and work
+            together to get out of here.  We need to be working together as a team\"
+            You see Xanarra relax slightly, some of the tension bleeding out of her.
+            """
+            continue_game2()
+
+        else:
+            print "Answer the question, stupid."
+
+
+def continue_game():
+    cont_inue = raw_input("Press enter to continue\n> ")
+    if cont_inue == "":
+        journey_2()
+    else:
+        print "Something went terribly wrong..."
+        exit(0)
+
+def continue_game2():
+    cont_inue = raw_input("Press enter to continue\n> ")
+    if cont_inue == "":
+        door()
+    else:
+        print "Something went terribly wrong..."
+        exit(0)
+
+def door():
+    print "This is where door will go"
     exit(0)
 
 
